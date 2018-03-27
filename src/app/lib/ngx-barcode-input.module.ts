097dc16639eb4c4d74ecdaa98e4908bf7e92c174
neojato <kpaul86+github@gmail.com>
2018-03-27 05:07:49 -0500

    update(app): include bootstrap and Quagga libs

--- src/app/lib/ngx-barcode-input.module.ts
+++ src/app/lib/ngx-barcode-input.module.ts
@@ -2,10 +2,12 @@ import { InputRefDirective } from './common/input-ref.directive';
 import { NgxBarcodeInputComponent } from './ngx-barcode-input/ngx-barcode-input.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModalModule } from 'ngx-bootstrap';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ModalModule.forRoot()
   ],
   declarations: [NgxBarcodeInputComponent, InputRefDirective],
   exports: [NgxBarcodeInputComponent, InputRefDirective]
